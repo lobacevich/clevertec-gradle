d87991dc5931103d7c584f4d84664760b1f47923
Igor <lobacevich_@tut.by>
2023-10-19 01:20:12 +0300

    feat: add module api

---
+++ api/src/main/java/by/clevertec/lobacevich/Main.java
@@ -0,0 +1,8 @@
+package by.clevertec.lobacevich;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Utils.isAllPositiveNumbers("12", "79"));
+    }
+}
